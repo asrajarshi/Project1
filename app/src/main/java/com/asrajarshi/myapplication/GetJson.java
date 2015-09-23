98025342b249030cbb636c18e7f33819af488da2
asrajarshi <asr471@nyu.edu>
2015-09-23 08:30:36 -0400

    improved datastructure

--- app/src/main/java/com/asrajarshi/myapplication/GetJson.java
+++ app/src/main/java/com/asrajarshi/myapplication/GetJson.java
@@ -7,6 +7,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
@@ -15,18 +16,13 @@ import java.util.List;
  */
 public class GetJson {
     String json;
-    String data = "";
-    int countt;
-    ArrayList<String> frmula = new ArrayList<>();
-    ArrayList<String> urrl = new ArrayList<>();
-    ArrayList<String> ab = new ArrayList<>();
-    ArrayList<String> ab1 = new ArrayList<>();
     List<ArrayList<String>> ab11 = new ArrayList<>();
     int count;
 
     GetJson(String json) {
         this.json = json;
     }
+
     public List<ArrayList<String>> getIt() throws JSONException {
         JSONObject reader = new JSONObject(json);
         //Get the instance of JSONArray that contains JSONObjects
@@ -35,22 +31,18 @@ public class GetJson {
         ArrayList<String> key = gotIt(jsonArray);
         int cn = getCount();
         for (int i = 0; i < jsonArray.length(); i++) {
+            ArrayList<String> ab1 = new ArrayList<>();
             JSONObject jsonObject = jsonArray.getJSONObject(i);
             String[] value = new String[cn];
-            for(int j =0;j<cn;j++)
-                value[j] = jsonObject.optString(key.get(j)).toString(); // to retrive elemtns in one object
-            frmula.add((value[0])); //add value to 1st column
-            ab.add(value[1]); // add value to second colum
-            ab1.add((value[2])); // add to third column
-            urrl.add(value[3]); // add to forth column
+            for(int j =0;j<cn;j++){
+                value[j] = jsonObject.optString(key.get(j)).toString(); // to retrive elemtns in one object, if used hashmap I could have put map.getValue
+                ab1.add((value[j])); //add value to 1st column//
+            }
+            ab11.add(ab1);
         }
-        ab11.add(urrl); //add 1st column to the list
-        ab11.add(frmula); // add 2nd column to the list
-        ab11.add(ab); // add 3rd column to the list
-        ab11.add(ab1); // add 4th column to the lsit
         return ab11;
     }
-// This method is to iterate over keys in one json object and to return those keys
+    // This method is to iterate over keys in one json object and to return those keys
     public ArrayList<String > gotIt(JSONArray jsonArray) throws JSONException {
         JSONObject jsonObject = jsonArray.getJSONObject(1);
         Iterator iterator = jsonObject.keys();
@@ -63,12 +55,5 @@ public class GetJson {
     }
     public int getCount(){
         return count;
-    }
-
-    public void setCountt(int countt) {
-        this.countt = countt;
-    }
-    public int getCounttt(){
-        return countt;
     }
 }
--- app/src/main/java/com/asrajarshi/myapplication/MainActivity.java
+++ app/src/main/java/com/asrajarshi/myapplication/MainActivity.java
@@ -17,13 +17,9 @@ import org.json.JSONException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
 public class MainActivity extends Activity {
-   Context appContext;
-    int i;
-    int jjj;
-    int rowCount;
-    Integer[] jj = new Integer[100];
+    static int i;
+    Context appContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,52 +35,50 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
-
-    // This method is called when a buttonis pressed
-        public void click1(View view) throws IOException {
-            TableLayout table = (TableLayout) findViewById(R.id.myTableLayout);
-            appContext = getBaseContext();
-            //To retrieve .json file from assets folder
-            GetAssets getFile = new GetAssets(appContext);
-            String bufferString = getFile.getFileAssets();
-            //Create insttance of GetJson: in method getIt() we can parse the json string
-            final GetJson getJson1 = new GetJson(bufferString);
-            //create list of Arraylists where each arraylist contain onr column
-            List<ArrayList<String>> ab;
-            try {
-                ab = getJson1.getIt(); // get the List of ArrayLists i.e. whole table
-                int size = ab.get(0).size(); // get rowcount
-                int nCol = ab.size(); // get column count
-                for (i = 0; i < size; i++) {
-                    TableRow row = new TableRow(this);
-                    row.setClickable(true);
-                    String[] debt = new String[nCol];
-                    for(int j =0;j<nCol ; j++){
-                        TextView[] text = new TextView[size];
-                        debt[j] = ab.get(j).get(i); // set row wise
-                        text[j] = new TextView(MainActivity.this);
-                        text[j].setText(debt[j]);
-                        text[j].setPadding(20, 10, 10, 10);
-                        row.addView(text[j]);
-                        getJson1.setCountt(i);
-                    }
-                    table.addView(row);
-                    row.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            jj[jjj] = rowCount;
-                            jjj++;
-                            rowCount++;
-                            int hh = getJson1.getCounttt();
-                            Log.d("mainacc", "hello rami");
-                            Toast.makeText(MainActivity.this, "Discount should be less than 100"+hh,Toast.LENGTH_SHORT).show();
+    public void click1(View view) throws IOException {
+        TableLayout table = (TableLayout) findViewById(R.id.myTableLayout);
+        appContext = getBaseContext();
+        //To retrieve .json file from assets folder
+        GetAssets getFile = new GetAssets(appContext);
+        String bufferString = getFile.getFileAssets();
+        GetJson getJson1 = new GetJson(bufferString);
+        //create list of Arraylists where each arraylist contain onr column
+        List<ArrayList<String>> ab= new ArrayList<ArrayList<String>>();
+        try {
+            ab = getJson1.getIt(); // get the List of ArrayLists
+            int nCol = ab.get(0).size(); //size of column
+            int nRow = ab.size(); //size of row
+            for (i = 0; i < nRow; i++) {
+                TableRow row = new TableRow(this);
+                //TableRow row = new TableRow(this);
+                row.setClickable(true);
+                String[] debt = new String[nCol];
+                for(int j =0;j<nCol ; j++){
+                    TextView[] text = new TextView[nCol];
+                    debt[j] = ab.get(i).get(j);
+                    text[j] = new TextView(MainActivity.this);
+                    text[j].setText(debt[j]);
+                    text[j].setPadding(5, 2, 2, 2);
+                    row.addView(text[j]);
+                }
+                table.addView(row);
+                row.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Log.d("mainacc", "hello rami");
+                            Toast.makeText(MainActivity.this, "number is "+i,Toast.LENGTH_SHORT).show();
                         }});
                 } }catch (JSONException e) {
                 e.printStackTrace();
